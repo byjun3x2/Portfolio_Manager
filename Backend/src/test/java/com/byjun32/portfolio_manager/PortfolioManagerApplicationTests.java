03d81d4603219d7c81b32d9081fe505364a53d49
byjun3x2 <byjun32@gmail.com>
2025-07-07 17:18:40 +0900

    feat: initial backend setup

---
+++ Backend/src/test/java/com/byjun32/portfolio_manager/PortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byjun32.portfolio_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
